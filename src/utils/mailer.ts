f8ffc3a59ebdbed2280d68ba9524ef2609b6c0fb
mxsgx <mxsgx.id@gmail.com>
2022-05-23 21:45:37 +0700

    fix: misconfigured SMTP

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: +process.env.SMTP_HOST,
+  port: +process.env.SMTP_PORT,
   secure: false,
   auth: {
     user: process.env.SMTP_USER,
